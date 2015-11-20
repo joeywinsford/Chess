ce0570343eca1ec26ef4f0f3e9b1f03ab82eef5d
Joe Lloyd <joe.lloyd@advancedcomputersoftware.com>
2015-11-20 11:42:42 +0000

    Sessions accept inputs

---
+++ Chess/Session.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Chess
+{
+    public class Session
+    {
+        public Session()
+        {
+        }
+
+        public List<Input> Inputs { get; } = new List<Input>();
+
+        public void ReceiveInput(Input input)
+        {
+            Inputs.Add(input);
+        }
+    }
+
+    public class Input
+    {
+        public Input(string command)
+        {
+            Command = command;
+        }
+
+        public string Command { get; private set; }
+    }
+}
---
+++ ChessTests/StartingANewGameSession.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Chess;
+using Xunit;
+
+namespace ChessTests
+{
+    public class StartingANewGameSession
+    {
+        [Fact]
+        public void SessionAcceptsInput()
+        {
+            var command = "Start Game";
+            var session = new Session();
+
+            session.ReceiveInput(new Input(command));
+
+            Assert.Equal(command, session.Inputs.First().Command);
+        }
+    }
+}
