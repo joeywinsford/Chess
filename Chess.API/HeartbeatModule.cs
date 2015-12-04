7d763b49ba903400d6f112fe6f65cc02bd5dd06a
Joe Lloyd <joe.lloyd@advancedcomputersoftware.com>
2015-12-04 15:04:27 +0000

    Add new project for web API.  Include publish settings for Azure.

---
+++ Chess.API/HeartbeatModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Nancy;
+
+namespace Chess.API
+{
+    public class HeartbeatModule : NancyModule
+    {
+        public HeartbeatModule()
+        {
+            Get["/"] = parameters => $"The time on the server is {DateTime.Now.ToString("R")}";
+        }
+    }
+}
---
+++ Chess.API/Start.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Owin;
+
+namespace Chess.API
+{
+    public class Start
+    {
+        public void Configure(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
